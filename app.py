5286e4dcc6c55e66111bed45bebdcaa29d770e37
Krizt-ai <krrizt054@gmail.com>
2026-06-03 16:21:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 app.secret_key = "newspoint_secret"
 
 # ================= DATABASE =================
-app.config['MYSQL_HOST'] = os.getenv('mysql.railway.internal')
-app.config['MYSQL_USER'] = os.getenv('root')
-app.config['MYSQL_PASSWORD'] = os.getenv('YpnUTzgOOcaStzXoyFOvoiSmVyjjkjhy')
-app.config['MYSQL_DB'] = os.getenv('railway')
-app.config['MYSQL_PORT'] = int(os.getenv('3306', 3306))
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 
 
 mysql = MySQL(app)
